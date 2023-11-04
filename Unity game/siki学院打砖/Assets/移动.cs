776528d1288db2c81c3b73799d2d6abaafc62dc1
Yuik520 <149876307+Yuik520@users.noreply.github.com>
2023-11-04 20:03:45 +0800

    Add files via upload

---
+++ Unity game/siki学院打砖/Assets/移动.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 移动 : MonoBehaviour
+{
+    public Rigidbody qiu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        qiu = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //qiu.AddForce(Vector3.right);
+        //(x,y,z)(0,0,0) ;
+        //forward(前进);back(后退); right(右);left(左);up(向上)
+        //qiu.AddForce(new Vector3(10, 0, 0));
+
+        
+            
+        float X = Input.GetAxis("Horizontal");
+        //float(赋值一个小数) Input(输入)；GetAxis(轴"水平轴")；Horizontal(左右水平)
+        float Z = Input.GetAxis("Vertical");
+        Debug.Log(X);
+        qiu.AddForce(new Vector3(X, 0, Z) * 2);
+    }
+}
---
+++ Unity game/siki学院打砖/Assets/跟随.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 跟随 : MonoBehaviour
+{ public Transform playTransform;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {//使相机移动的位置的距离=自身的位置-移动的位置
+         offset = transform.position - playTransform.position; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playTransform.position  + offset;
+    }
+}

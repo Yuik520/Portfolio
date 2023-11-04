26bf8e2457a5e32e9d657f69ac9fe910c29adfe6
Yuik520 <149876307+Yuik520@users.noreply.github.com>
2023-11-04 20:24:11 +0800

    Add files via upload

---
+++ Unity game/吃豆人/Assets/Scripts/ZHUANG.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//第二关转场
+public class ZHUANG : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void jump()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Jump()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
